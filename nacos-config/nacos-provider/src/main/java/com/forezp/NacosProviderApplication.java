7cffacef4937872d3789d44c18c9ecdf7a7167c4
VinoClare <vinoclare7@gmail.com>
2022-08-11 10:39:14 +0800

    MySQL 连接成功

--- nacos-config/nacos-provider/src/main/java/com/forezp/NacosProviderApplication.java
+++ nacos-config/nacos-provider/src/main/java/com/forezp/NacosProviderApplication.java
@@ -1,11 +1,15 @@
 package com.forezp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+
+@MapperScan("com.forezp.Mapper")
+
 public class NacosProviderApplication {
 
 	public static void main(String[] args) {
