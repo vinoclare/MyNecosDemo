6cb3841fada6cd5c743fbe0d60896b31731cfbba
VinoClare <vinoclare7@gmail.com>
2022-08-12 15:07:20 +0800

    新增两个服务

--- ZuulDemo/discover-service/src/main/java/com/example/demo/DiscoverServiceApplication.java
+++ ZuulDemo/discover-service/src/main/java/com/example/demo/DiscoverServiceApplication.java
@@ -11,9 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 public class DiscoverServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoverServiceApplication.class, args);
 	}
-
 }
